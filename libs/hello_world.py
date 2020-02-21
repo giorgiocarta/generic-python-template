28b80c2c344f0b24957590164e854264ed09c1d9
Giorgio Carta <giorgio.carta@distilled.ie>
2020-02-21 12:58:50 +0000

    added attribute and method for tutorial

--- libs/hello_world.py
+++ libs/hello_world.py
@@ -3,5 +3,11 @@ from os import getenv
 
 class HelloWorld:
 
+    def __init__(self):
+        self.my_attribute = "whatever"
+
     def greet(self) -> str:
         return "{}".format(getenv('TEST_STRING', "Env file not loaded"))
+
+    def capitalize(self, word: str) -> str:
+        return word.capitalize()
